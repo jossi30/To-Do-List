846d199ca3f51da8b3571a4996da87767f3e071f
Yosief G. Hailay <yosiefghaylay@gmail.com>
2023-04-06 10:13:46 +0300

    modified the function of the project by adding modules

--- src/index.js
+++ src/index.js
@@ -1,29 +1,67 @@
 import './style.css';
 
-const itemsList = [{
-  index: 0,
-  description: 'Finish the to-do list',
-  completed: false,
-},
-{
-  index: 1,
-  description: 'Clean the house',
-  completed: false,
-},
-{
-  index: 2,
-  description: 'wash the dishes',
-  completed: true,
-},
-];
+import {
+  addToDo, editToDo, deleteToDo, clearToDos, markToDo,
+} from './modules/todos.js';
 
-const toDoListContainer = document.querySelector('.to-do-list-container');
+import { setToDo, getToDo as toDoItems } from './modules/store.js';
+import { renderToDoList } from './modules/render.js';
 
-const itemsListDisplay = () => {
-  itemsList.forEach((item) => {
-    toDoListContainer.innerHTML += `
-    <li class='to-do-item' value='index'>${item.description}<i class="fa-solid fa-ellipsis-vertical"></i></li>`;
-  });
+// Variables
+const input = document.querySelector('.input-field');
+const toDoList = document.querySelector('.todo-container');
+const addButton = document.querySelector('.add-btn');
+
+const clearButton = document.querySelector('.clear-all-btn');
+
+// Add Event Listener with Enter
+input.addEventListener('keypress', (e) => {
+  if (e.keyCode === 13 && input.value !== '') {
+    addToDo(toDoItems(), input.value);
+    input.value = '';
+    renderToDoList(toDoItems());
+  }
+});
+
+// Add Event Listener with Click
+addButton.addEventListener('click', () => {
+  if (input.value !== '') {
+    addToDo(toDoItems(), input.value);
+    input.value = '';
+    setToDo(toDoItems());
+    renderToDoList(toDoItems());
+  }
+});
+
+// Edit Event Listener
+toDoList.addEventListener('click', (e) => {
+  if (e.target.closest('.todo-text')) {
+    editToDo(e, toDoItems());
+  }
+});
+
+// Delete Event Listener
+toDoList.addEventListener('click', (e) => {
+  if (e.target.closest('.trash-can')) {
+    deleteToDo(e, toDoItems());
+  }
+});
+
+// Clear Completed Todo Event Listener
+clearButton.addEventListener('click', () => {
+  const toDoList = clearToDos(toDoItems());
+  setToDo(toDoList);
+  renderToDoList(toDoItems());
+});
+
+// Mark Completed Todo Event Listener
+toDoList.addEventListener('click', (e) => {
+  if (e.target.closest('.todo-checkbox')) {
+    markToDo(e, toDoItems());
+  }
+});
+
+const render = () => {
+  window.addEventListener('load', renderToDoList(toDoItems()));
 };
-
-window.addEventListener('load', itemsListDisplay);
+render();
